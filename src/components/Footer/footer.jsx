65e1561c85a0777e91fa7d1f3cf33049865826ac
robertaliliane <robertalilianesoares@gmail.com>
2024-12-04 19:05:29 -0300

    AtTwitter

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,7 +2,7 @@ import S from './footer.module.scss'
 import facebook from '../../assets/images/facebook.png'
 import instagram from '../../assets/images/instagram.png'
 import linkedin from '../../assets/images/linkedin.png'
-import twitter from '../../assets/images/twitter.png'
+import twitter from '../../assets/images/Twitter.png'
 import youtube from '../../assets/images/youtube.png'
 
 
@@ -14,7 +14,7 @@ export default function footer () {
         <h3>4002-8922</h3>
         <nav>
    <a href="">  <img src={facebook} alt="logo do facebook" />  </a>
-   <a href="">  <img src={twitter} alt="logo do twitter" />  </a>
+   <a href="">  <img src={twitter} alt="logo do Twitter" />  </a>
    <a href="">  <img src={youtube} alt="logo do youtube" /> </a>
    <a href="">  <img src={linkedin} alt="logo do linkedin" />  </a>
    <a href="">  <img src={instagram} alt="logo do instagram" />  </a>
