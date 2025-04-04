21096dd9b3af5f2d4dacb7e87d385445bee6bb72
robertaliliane <robertalilianesoares@gmail.com>
2025-04-04 23:22:22 +0200

    Atualizando

--- src/Pages/LivrosDoados/livrosdoados.jsx
+++ src/Pages/LivrosDoados/livrosdoados.jsx
@@ -1,19 +1,45 @@
 import S from './livrosdoados.module.scss'
 import livroprotagonista from '../../assets/images/livroprotagonista.png'
+import axios from 'axios'
+import { useState,useEffect } from 'react'
 
 export default function LivrosDoados (){
+
+const [livros, setLivros] = useState([])
+
+const getLivros = async () => {
+
+const response = await axios.get("https://desafio-api2.onrender.com/livros")
+setLivros(response.data)
+
+
+useEffect(()=>{
+    getLivros()
+},[])
+
+}
+
     return(
     <section className={S.boxLivrosDoados}>
         <h2>Livros Doados</h2>
-        <section className={S.boxCard}>
+        <section className={S.boxcard}>
                 <article>
-                    <img src={livroprotagonista} alt="imagem de um livro que se chama o protagonista" />
-                    
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                    
+                <img src={livroprotagonista} alt="imagem de um livro que se chama o protagonista" />
+                <h3>O Protagonista</h3>
+                <p>Susanne Andrade</p>
+                <p>Ficção</p>
                 </article>
+                {livros.map((item)=>(
+              <article key={item.id}>
+               <img src={item.imagem_url} alt="" />
+                <h3>{item.titulo}</h3>
+                <p>{item.categoria}</p>
+                <p>{item.autor}</p>
+              </article>
+
+                ))}
+
+
                 </section>
         </section>
     )
--- src/Pages/QueroDoar/querodoar.jsx
+++ src/Pages/QueroDoar/querodoar.jsx
@@ -1,21 +1,68 @@
 import S from './querodoar.module.scss'
 import livro from '../../assets/images/livro.png'
+import axios from 'axios'
+import {useState} from 'react'
 
 export default function QueroDoar(){
+
+ const [titulo, setTitulo] = useState("")
+ const [categoria, SetCategoria] = useState("")
+ const [autor, SetAutor] = useState("")
+ const [imagem_url, SetImagem_url] = useState("")
+
+ const enviarDados= async()=>{
+
+ const urlApi = "https://desafio-api2.onrender.com/doar"
+
+ const dadosEnviar = {
+    titulo, 
+    categoria,
+    autor,
+    imagem_url
+ }
+
+await axios.post(urlApi,dadosEnviar)
+
+alert('Livro enviado')
+
+setTitulo("")
+SetCategoria("")
+SetAutor("")
+SetImagem_url("")
+
+ }
+
+
+const capturaTitulo = (e) =>{
+    setTitulo(e.target.value)
+}
+
+const capturaCategoria = (e) =>{
+    SetCategoria(e.target.value)
+}
+
+const capturaAutor = (e) =>{
+    SetAutor(e.target.value)
+}
+
+const capturaImagem = (e) =>{
+    SetImagem_url(e.target.value)
+}
+
     return(
         <section className={S.principal}>
             <section className={S.container}>
                 <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
-                <form action="">
+                <form onSubmit={(e) => e.preventDefault()}>
                   <div>
                     <img src={livro} alt="icone de um livro" />
                     <h3>Informações do Livro</h3>
                     </div>  
-                    <input type="text" placeholder='Titulo' />
-                    <input type="text" placeholder='Categoria' />
-                    <input type="text" placeholder='Autor' />
-                    <input type="text" placeholder='Link da Imagem' />
-                    <input type="submit" value="Doar" />
+                    <input type="text" placeholder='Titulo' onChange={capturaTitulo} value={titulo} />
+                    <input type="text" placeholder='Categoria' onChange={capturaCategoria} value={categoria}/>
+                    <input type="text" placeholder='Autor' onChange={capturaAutor} value={autor} />
+                    <input type="text" placeholder='Link da Imagem' onChange={capturaImagem} value={imagem_url} />
+                    <input type="submit" value="Doar" onClick={enviarDados} />
                 </form>
             </section>
         </section>
